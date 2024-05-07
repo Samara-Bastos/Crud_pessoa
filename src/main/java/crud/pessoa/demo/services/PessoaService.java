fdbbfd3a66999de4e394ed42092081ee1198fee5
Samara Bastos <samarabastos.dev@gmail.com>
2024-05-07 12:45:53 -0300

    Adcionada a função para atualizar o registro de pessoa

--- src/main/java/crud/pessoa/demo/services/PessoaService.java
+++ src/main/java/crud/pessoa/demo/services/PessoaService.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 import java.util.List;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import crud.pessoa.demo.DTO.PessoaListDTO;
+import crud.pessoa.demo.dto.PessoaListDTO;
 import crud.pessoa.demo.exceptions.CreatePessoaException;
 import crud.pessoa.demo.models.Pessoa;
 import crud.pessoa.demo.repository.PessoaRepository;
@@ -14,7 +16,6 @@ import jakarta.transaction.Transactional;
 
 @Service
 public class PessoaService {
-
 
     private Logger logger = Logger.getLogger(PessoaService.class.getName());
 
@@ -26,12 +27,12 @@ public class PessoaService {
     public Pessoa create(Pessoa pessoa){
         logger.info("Inserção de uma pessoa");
 
-        // Optional<Pessoa> pessoaReturn = findByPessoaCpf(pessoa.getCpf());
+        Optional<Pessoa> pessoaReturn = findByPessoaCpf(pessoa.getCpf());
         
-        // if(pessoaReturn.isPresent()){
-        //     throw new CreatePessoaException("Já existe uma pessoa cadastrada com esse CPF");
+        if(pessoaReturn.isPresent()){
+            throw new CreatePessoaException("Já existe uma pessoa cadastrada com esse CPF");
             
-        // }
+        }
     
         return repository.save(pessoa);
     }
@@ -42,13 +43,35 @@ public class PessoaService {
     }
 
 
-    public List<PessoaListDTO> findAll() {
+    public Page<Object> findAll(Pageable paginacao) {
         logger.info("Pessoas cadastradas");
-		return repository.findAll().stream().map(PessoaListDTO::new).toList();
+		return repository.findAll(paginacao).map(PessoaListDTO::new);
 	}
 
     public Pessoa findById(Long id) {
 		logger.info("Pessoa");
         return repository.findById(id).orElseThrow(() -> new CreatePessoaException("Pessoa não encontrada")); 
 	}
+
+    
+    @Transactional //Anotação para fazer uma ligação direta com o banco de dados
+    public Pessoa update(String cpf, Pessoa pessoa){
+        logger.info("Atualização de uma pessoa");
+
+        Optional<Pessoa> pessoaReturn = findByPessoaCpf(cpf);
+        
+        if(pessoaReturn.isEmpty()){
+            throw new CreatePessoaException("Pessoa não encontrada");
+            
+        }
+
+        Pessoa pessoaNova = pessoaReturn.get();
+
+        pessoaNova.setNome(pessoa.getNome());
+        pessoaNova.setNascimento(pessoa.getNascimento());
+        pessoaNova.setCpf(pessoa.getCpf());
+    
+        return repository.save(pessoaNova);
+    }
+
 }
