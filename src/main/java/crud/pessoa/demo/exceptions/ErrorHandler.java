bb5872faad9c57a9eb36c8a6fb299687e7fded15
Samara Bastos <samarabastos.dev@gmail.com>
2024-05-14 16:30:19 -0300

    Adcionada a tratativa da exception 'FindPessoaException'

--- src/main/java/crud/pessoa/demo/exceptions/ErrorHandler.java
+++ src/main/java/crud/pessoa/demo/exceptions/ErrorHandler.java
@@ -17,5 +17,8 @@ public class ErrorHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
-    
+    @ExceptionHandler(FindPessoaException.class)
+    public ResponseEntity<String> handleErrorFindPessoaException(FindPessoaException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
 }
