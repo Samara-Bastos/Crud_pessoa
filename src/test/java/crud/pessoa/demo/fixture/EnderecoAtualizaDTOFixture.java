509635e7dd466fac1297708a3699959cc07b6eee
Samara Bastos <samarabastos.dev@gmail.com>
2024-05-22 14:53:19 -0300

    Aletarado o tipo do do atributo  'numero'

--- src/test/java/crud/pessoa/demo/fixture/EnderecoAtualizaDTOFixture.java
+++ src/test/java/crud/pessoa/demo/fixture/EnderecoAtualizaDTOFixture.java
@@ -4,10 +4,10 @@ import crud.pessoa.demo.dto.EnderecoAtualizaDTO;
 
 public class EnderecoAtualizaDTOFixture {
     public static EnderecoAtualizaDTO EnderecoAtualizaDTOValido() {
-        return new EnderecoAtualizaDTO("Rua A", 123, "Centro", "Cidade A", "Estado A", "12345678", true);
+        return new EnderecoAtualizaDTO("Rua A", "123", "Centro", "Cidade A", "Estado A", "12345678", true);
     }
 
     public static EnderecoAtualizaDTO EnderecoAtualizaDTOInvalido() {
-        return new EnderecoAtualizaDTO("", 123, "Centro", "Cidade A", "Estado A", "12345678", true);
+        return new EnderecoAtualizaDTO("", "123", "Centro", "Cidade A", "Estado A", "12345678", true);
     }
 }
--- src/test/java/crud/pessoa/demo/fixture/EnderecoDTOFixture.java
+++ src/test/java/crud/pessoa/demo/fixture/EnderecoDTOFixture.java
@@ -5,10 +5,10 @@ import crud.pessoa.demo.dto.EnderecoDTO;
 public class EnderecoDTOFixture {
      
     public static EnderecoDTO EnderecoDTOValido() {
-        return new EnderecoDTO("Rua A", 123, "Centro", "Cidade A", "Estado A", "12345678", "30143518062", true);
+        return new EnderecoDTO("Rua A", "123", "Centro", "Cidade A", "Estado A", "12345678", "30143518062", true);
     }
 
     public static EnderecoDTO EnderecoDTOInvalido() {
-        return new EnderecoDTO("", 123, "Centro", "Cidade A", "Estado A", "12345678", "30143518062", true);
+        return new EnderecoDTO("", "123", "Centro", "Cidade A", "Estado A", "12345678", "30143518062", true);
     }
 }
